2e03667aa43d7a4cca27ae8de6df9668c6fc50aa
Cyclone35Jokesays <pesasebastian@gmail.com>
2020-01-30 22:07:49 -0800

    Work in progres - encountered new bugs

---
+++ Assets/Scripts/Interactions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    public Transform friendTransform;
+    public Animator anim;
+    private bool wavingBack;
+
+    void Update()
+    {
+        StartCoroutine(WaveBack());
+    }
+
+    private IEnumerator WaveBack()
+    {
+        wavingBack = true;
+        if (Vector3.Distance(playerTransform.position, friendTransform.position) < 100f)
+        {
+            anim.SetTrigger("IsWavingBack");
+        }
+        yield return new WaitForSeconds(3);
+        wavingBack = false;
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public GameObject friend;
+    public Animator anim;
+    public ParticleSystem effects;
+    private bool wave;
+    private float waveDuration = 3f;
+    private float acceleration = 10;
+    private float currentTargetSpeed = 1f;
+
+
+    private void Update()
+    {
+        Vector3 moveInput = new Vector3(speed * Input.GetAxisRaw("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxisRaw("Vertical") * Time.deltaTime);
+
+        if (moveInput != new Vector3(0, 0, 0))
+        {
+            anim.SetBool("IsWalking", true);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+    
+        transform.Translate(moveInput * speed * Time.deltaTime, Space.World);
+        if (moveInput != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveInput), 0.1f);
+        }
+
+        if (!wave)
+            speed = Mathf.MoveTowards(speed, currentTargetSpeed, acceleration * Time.deltaTime);
+
+        if (!wave && Input.GetMouseButtonDown(0))
+        {
+            speed = 0;
+            StartCoroutine(Wave());
+        }
+
+        IEnumerator Wave()
+        {
+            wave = true;
+            effects.GetComponent<ParticleSystem>().Play();
+            GetComponent<Animator>().SetTrigger("IsWaving");
+
+            if (Vector3.Distance(GetComponent<Transform>().position, friend.GetComponent<Transform>().position) < 100f)
+            {
+                effects.GetComponent<ParticleSystem>().Play();
+                friend.GetComponent<Animator>().SetTrigger("IsWaving");
+            }
+
+            yield return new WaitForSeconds(waveDuration);
+            wave = false;
+        }
+    }
+
+   
+}
